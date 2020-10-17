dacfdc861d3eaef88e22e317c2986685da8dec46
Micah Bowie <micahbowie20@gmail.com>
2020-10-17 12:12:03 -0400

    adding CRUD funstions to notes.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,11 +1,40 @@
 class NotesController < ApplicationController
 
   def index
+    @notes = Note.all.order("created_at DESC")
   end
 
-  def new 
+  def new
+    @note = Note.new
+
   end
 
   def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to @note
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    render 'edit'
+  end
+
+  def edit
+  end
+
+
+  private
+
+    def note_params
+      params.require(:note).permit(:title, :content)
+    end
 end
