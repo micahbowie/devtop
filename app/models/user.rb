19dff2679c7a9524327ea7ac2465108e39f66593
Micah Bowie <micahbowie20@gmail.com>
2020-12-06 09:27:51 -0500

    Updated user model with validations and dependencies.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :notes
-  has_many :flashcards
+  has_many :notes, dependent: :destroy
+  has_many :flashcards, dependent: :destroy
   has_many :questions
   has_many :answers
-  has_many :ideas
+  has_many :ideas, dependent: :destroy
   has_many :projects, through: :ideas
-  validates :username,      presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true,
+      :format => {
+      with: /[a-zA-Z0-9]+\z/ , 
+      :message => 'no special characters, only letters and numbers'
+      }
+  validates_length_of :username, minimum: 5, maximum: 12, allow_blank: false
 end
