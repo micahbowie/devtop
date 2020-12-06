069623e1f06d136c236624a1a2fa9f879d1ee802
Micah Bowie <micahbowie20@gmail.com>
2020-12-06 09:28:55 -0500

    Updated answer model with validations.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
-  validates :answer_content,      presence: true
+  validates :answer_content,      presence: true, length: { maximum: 500 }
   validates :user_id,      presence: true
 end
