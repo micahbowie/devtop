47f76ec5d8026b30f767c7a3e78f8a412caa7391
Micah Bowie <micahbowie20@gmail.com>
2020-12-06 10:36:53 -0500

    updated notes model

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,5 @@ class Note < ApplicationRecord
   belongs_to :user
   validates :title,      presence: true, length: { maximum: 20 }
   validates :content,      presence: true, length: { maximum: 500 }
+
 end
