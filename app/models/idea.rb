ae7fb8f3b2313305789c4fbd1ca2b30e55f69636
Micah Bowie <micahbowie20@gmail.com>
2020-12-08 19:58:27 -0500

    created validation and scope for idea class

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,4 +3,8 @@ class Idea < ApplicationRecord
   belongs_to :project
 
   validates :idea_content, presence: true, length: { maximum: 500 }
+  validates :project_id, presence: true
+  scope :cli_ideas, -> { where(project_id: 5) }
+  scope :sinatra_ideas, -> { where(project_id: 4) }
+  scope :rails_ideas, -> { where(project_id: 3) }
 end
