ae7e7e64a47eae99ea83199b95d77b19d5701e78
Micah Bowie <micahbowie20@gmail.com>
2020-12-08 19:57:59 -0500

    created an index method, added flash messages, and rendered layout

--- app/controllers/projects/ideas_controller.rb
+++ app/controllers/projects/ideas_controller.rb
@@ -1,22 +1,37 @@
 class Projects::IdeasController < ApplicationController
 before_action :redirect_if_not_logged_in
+layout "idea-layout"
   def create
     @idea = Idea.new(idea_params)
     @idea.user_id = current_user.id
     if @idea.save
       redirect_to project_path(@idea.project_id)
     else
-      render 'new'
+      flash[:messages] = @idea.errors.full_messages
+      # byebug
+      redirect_to :new_project_idea
     end
   end
 
   def new
     @idea = Idea.new
   end
-end
+
+  def index
+    @cli_ideas = Idea.cli_ideas
+    @sinatra_ideas = Idea.sinatra_ideas
+    @rails_ideas = Idea.rails_ideas
+  end
+
+  def show
+    @idea = Idea.find(params[:id])
+  end
+
 
 private
 
   def idea_params
     params.permit(:idea_content, :project_id)
   end
+
+end
